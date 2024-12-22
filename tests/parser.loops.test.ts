24118210b1d4b7cb7f4ef1ca7f5ac71b8e35c5c3
Chris Vaszauskas <chrisvaszauskas@axiscare.com>
2024-12-22 16:00:01 -0600

    Finish for support

--- tests/parser.loops.test.ts
+++ tests/parser.loops.test.ts
@@ -110,5 +110,29 @@ describe('for', () => {
     expect(ast).toEqual(expected);
   });
 
-  test.todo('for (var i: number = 0; i < 10; i = i + 1) {2;}');
+  test('for (var i: number = 0; i < 10; ++i) {2;}', () => {
+    let source = 'for (var i: number = 0; i < 10; ++i) {2;}';
+    let expected = [
+      new stmt.For(
+        new stmt.Var(
+          new Token(TokenType.IDENTIFIER, 'i', undefined, 1),
+          new types.Number(),
+          new expr.NumberLiteral(0),
+        ),
+        new expr.Binary(
+          new expr.Variable(new Token(TokenType.IDENTIFIER, 'i', undefined, 1)),
+          new Token(TokenType.LESS, '<', undefined, 1),
+          new expr.NumberLiteral(10),
+        ),
+        new expr.Unary(
+          new Token(TokenType.PLUS_PLUS, '++', undefined, 1),
+          new expr.Variable(new Token(TokenType.IDENTIFIER, 'i', undefined, 1)),
+        ),
+        new stmt.Block([new stmt.Expression(new expr.NumberLiteral(2))]),
+      ),
+    ];
+    let tokens = scan(source);
+    let ast = parse(tokens);
+    expect(ast).toEqual(expected);
+  });
 });
