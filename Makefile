a53fc6e9d09ca8cb2b50a26b85318f5101d32d39
Chris Vaszauskas <chrisvaszauskas@axiscare.com>
2025-01-10 14:44:07 -0600

    Add code editor

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@ prettier:
 	npx prettier --write **/*.ts
 
 build:
-	rm -f *.js *.html *.css
+	rm -f *.js *.html *.css *.ttf
 	bun build --experimental-html --experimental-css ./web/index.html --outdir=.
--- web/main.ts
+++ web/main.ts
@@ -1,3 +1,23 @@
+import * as monaco from 'monaco-editor'
 import compile from '../src/compiler'
+import { PhpPrinter } from '../src/phpPrinter/print'
 
-console.log(compile('echo "Hello, World!";'))
+let phin = monaco.editor.create(document.querySelector('#phin')!, {
+  value: `echo "hello world"`,
+  language: 'kotlin',
+  minimap: { enabled: false },
+})
+let php = monaco.editor.create(document.querySelector('#php')!, {
+  value: ``,
+  language: 'php',
+  minimap: { enabled: false },
+})
+
+let run = document.getElementById('run')!
+run.addEventListener('click', () => {
+  let src = phin.getValue()
+  let ast = compile(src)
+  let printed = new PhpPrinter().print(ast)
+  php.setValue('<?php\n' + printed)
+})
+run.click()
